ca49303a8057418463a7e2d8b4d797e41e9c5855
Fazel Momeni <fzl8747@gmail.com>
2023-02-19 19:19:04 +0330

    Decrease len key User _ Client

--- User/models.py
+++ User/models.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 def RandomString():
     from Config.Tools import RandomString as R
-    return R(150)
+    return R(35)
 
 class User(AbstractUser):
     name = models.CharField(max_length=100)
     domain = models.CharField(max_length=300)
-    key = models.CharField(max_length=150,default=RandomString,unique=True)
+    key = models.CharField(max_length=35,default=RandomString,unique=True)
